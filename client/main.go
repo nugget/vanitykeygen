f6a89fdeaed218c0b5405d1dd5edac20a87284de
David McNett <contact@nugget.info>
2025-01-01 09:57:51 -0600

    Still a global logger, but adding channels

--- client/main.go
+++ client/main.go
@@ -12,9 +12,29 @@ import (
 	"time"
 )
 
+var (
+	logger   *slog.Logger
+	logLevel *slog.LevelVar
+)
+
 type hit struct {
 	timestamp time.Time
 	sid       int
+	keyCount  int
+}
+
+type telemetry struct {
+	startTime time.Time
+	keyCount  int
+	hitCount  int
+}
+
+func newTelemetry() telemetry {
+	return telemetry{
+		startTime: time.Now(),
+		keyCount:  0,
+		hitCount:  0,
+	}
 }
 
 func (h hit) LogValue() slog.Value {
@@ -23,11 +43,6 @@ func (h hit) LogValue() slog.Value {
 		slog.Int("sid", h.sid),
 	)
 }
-
-var (
-	logger   *slog.Logger
-	logLevel *slog.LevelVar
-)
 
 func seeker(ctx context.Context, hits chan hit, sid int) {
 	logger := logger.With("sid", sid)
@@ -42,17 +57,43 @@ func seeker(ctx context.Context, hits chan hit, sid int) {
 		h := hit{
 			timestamp: time.Now(),
 			sid:       sid,
+			keyCount:  r,
 		}
 		hits <- h
 	}
 }
 
-var neverReady = make(chan struct{}) // never closed
+func displayStats(t *telemetry) {
+	wallTime := time.Now().Sub(t.startTime)
 
-func recordHit(h hit) error {
-	logger.Info("run select hit", "h", h)
+	hitRate := fmt.Sprintf("%0.02f", float64(t.hitCount)/float64(t.keyCount)*100)
+
+	logger.Info("Runtime Stats",
+		"runtime", wallTime,
+		"keyCount", t.keyCount,
+		"hitCount", t.hitCount,
+		"hitRate", hitRate,
+	)
+}
+
+func recordHit(h hit, runtimeStats *telemetry) error {
+	runtimeStats.hitCount++
+	runtimeStats.keyCount += h.keyCount
+
+	logger.Warn("run select hit", "h", h)
 
 	return nil
+}
+
+func setupLogger(ctx context.Context, stdout io.Writer) {
+	logLevel = new(slog.LevelVar)
+
+	handlerOptions := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+	handler := slog.NewTextHandler(stdout, handlerOptions)
+
+	logger = slog.New(handler)
 }
 
 // run is the real main, but one where we can exit with an error.
@@ -60,12 +101,7 @@ func run(ctx context.Context, stdout io.Writer, stderr io.Writer, getenv func(st
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
-	logLevel = new(slog.LevelVar)
-	logLevel.Set(slog.LevelInfo)
-	handlerOptions := &slog.HandlerOptions{
-		Level: logLevel,
-	}
-	logger = slog.New(slog.NewTextHandler(os.Stdout, handlerOptions))
+	setupLogger(ctx, stdout)
 
 	myFlags := flag.NewFlagSet("myFlags", flag.ExitOnError)
 
@@ -82,13 +118,16 @@ func run(ctx context.Context, stdout io.Writer, stderr io.Writer, getenv func(st
 	go seeker(ctx, hits, 2)
 	go seeker(ctx, hits, 3)
 
-	logger.Info("run begin for")
+	runtimeStats := newTelemetry()
+	statsTrigger := time.Tick(10 * time.Second)
 
 RunLoop:
 	for {
 		select {
+		case <-statsTrigger:
+			displayStats(&runtimeStats)
 		case h := <-hits:
-			err := recordHit(h)
+			err := recordHit(h, &runtimeStats)
 			if err != nil {
 				logger.Warn("unable to record hit",
 					"hit", h,
@@ -96,15 +135,13 @@ RunLoop:
 				)
 			}
 		case <-ctx.Done():
-			logger.Warn("interrupt detected",
-				"err", ctx.Err(),
-			)
 			stop()
 			break RunLoop
 		default:
 			time.Sleep(250 * time.Millisecond)
 		}
 	}
+	logger.Warn("interrupt detected", "err", ctx.Err())
 
 	return nil
 }
